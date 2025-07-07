3d1308e6357d3aea003bab4b03d928b6baed8350
Laurent <seres@seres.eu.org>
2025-07-08 00:02:32 +0200

    fixed nvim theme. fixed blocky warning and incompat. blocklists.

--- home/seres/neovim.nix
+++ home/seres/neovim.nix
@@ -1,12 +1,37 @@
 _: {
+  stylix.targets.nvf.enable = false;
+
   programs.nvf = {
     enable = true;
     settings = {
       vim = {
-        viAlias = false;
+        viAlias = true;
         vimAlias = true;
         lsp.enable = true;
         lsp.formatOnSave = true;
+        theme = {
+          enable = true;
+          name = "mini-base16";
+          style = "dark";
+          base16-colors = {
+            base00 = "#242424";
+            base01 = "#323232";
+            base02 = "#404040";
+            base03 = "#474747";
+            base04 = "#4e4e4e";
+            base05 = "#E6E6E6";
+            base06 = "#F5F5F5";
+            base07 = "#FFFFFF";
+            base08 = "#E3C401";
+            base09 = "#24DFC4";
+            base0A = "#13C299";
+            base0B = "#47B413";
+            base0C = "#FF4D51";
+            base0D = "#24ACD4";
+            base0E = "#F2AFFD";
+            base0F = "#E6E6E6";
+          };
+        };
         languages = {
           enableFormat = true;
           enableTreesitter = true;
--- modules/services/blocky.nix
+++ modules/services/blocky.nix
@@ -1,40 +1,59 @@
-_: {
-  services.blocky = {
-    enable = true;
-    settings = {
-      ports.dns = 53;
-      upstreams.groups.default = [
-        "https://dns.quad9.net/dns-query"
-      ];
-      bootstrapDns = {
-        upstream = "https://dns.quad9.net/dns-query";
-        ips = ["9.9.9.9" "149.112.112.112"];
-      };
-      blocking = {
-        blackLists = {
-          ads = [
-            "https://big.oisd.nl/domainswild"
-            "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.txt"
-            "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
-            "https://adguardteam.github.io/AdguardFilters/FrenchFilter/sections/adservers.txt"
-            "https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt"
-            "https://raw.githubusercontent.com/easylist/easylist/master/easyprivacy/easyprivacy_trackingservers.txt"
-          ];
-        };
-        clientGroupsBlock = {
-          default = ["ads"];
-        };
-      };
+{
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    blocky = {
+      openToLan = lib.mkEnableOption "enables access from LAN network.";
     };
   };
 
-  networking.nameservers = [
-    "127.0.0.1"
-    "9.9.9.9"
-  ];
+  config = {
+    services.blocky = {
+      enable = true;
+      settings = {
+        ports.dns = 53;
+        upstreams.groups.default = [
+          "https://dns.quad9.net/dns-query"
+        ];
+        bootstrapDns = {
+          upstream = "https://dns.quad9.net/dns-query";
+          ips = ["9.9.9.9" "149.112.112.112"];
+        };
+        blocking = {
+          denyLists = {
+            ads = [
+              "https://big.oisd.nl/domainswild"
+              "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.txt"
+              # NOTE: These aren't compatible
+              # "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
+              # "https://adguardteam.github.io/AdguardFilters/FrenchFilter/sections/adservers.txt"
+              # "https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt"
+              # "https://raw.githubusercontent.com/easylist/easylist/master/easyprivacy/easyprivacy_trackingservers.txt"
+            ];
+          };
+          clientGroupsBlock = {
+            default = ["ads"];
+          };
+        };
+      };
+    };
 
-  networking.networkmanager.insertNameservers = [
-    "127.0.0.1"
-    "9.9.9.9"
-  ];
+    networking = {
+      nameservers = [
+        "127.0.0.1"
+        "9.9.9.9"
+      ];
+
+      networkmanager.insertNameservers = [
+        "127.0.0.1"
+        "9.9.9.9"
+      ];
+    };
+
+    networking.firewall.allowedTCPPorts = lib.mkIf config.blocky.openToLan [
+      53
+    ];
+  };
 }
