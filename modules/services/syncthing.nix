5fab42ff830d8ce9fb28544fe6b53c291b775719
seres <seres@seres.eu.org>
2025-09-19 08:49:38 +0200

    updated syncthing config

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -6,6 +6,8 @@ _: {
       devices = {
         "rpi" = {id = "TON2NU7-TBSTUMF-5FPHZGC-SLGA2LZ-VNEMCB6-IQGN56B-KYESWLQ-I5HZTQV";};
         "Pixel 8a" = {id = "A5VDEUP-J23UWM6-OP4PHUL-FLFEXKD-KFNVUVV-A5U6U5W-PPS2MR6-NCYNMQ5";};
+        "io" = {id = "6C4JU65-7GAO2QK-BTJLI4I-673GVLB-XFUEARK-RX6FRXA-ANZVY4O-WHOCCAN";};
+        "nessus" = {id = "644TS26-NDPT7FP-OEHKA6D-72T6I4F-7CBLOJO-VBOLELS-DHFKYX6-G22H3AY";};
       };
       folders = {
         "s9cb7-umi75" = {
